3aee92542869f55d6946813839809f9c91cb530e
DmitryZagr <bikemaster2000@list.ru>
2017-07-03 21:47:04 +0300

    add docker assembly support

--- docker/scripts/assembly.sh
+++ docker/scripts/assembly.sh
@@ -7,9 +7,11 @@ root_dir=$script_dir/../..
 build_dir=$script_dir/../build 
 dockerfiles_dir=$script_dir/../dokerfiles
 bin_dir=$root_dir/products/org.eclipse.userstorage.product/target/products
+unzip_bin_dir=$build_dir/unzip_dir
 
 assembly_with_src=true
 bin_zip_template=*linux*64*.zip
+docker_file_name=Dockerfile
 
 critical_message()  # $1 - error message
 {
@@ -39,8 +41,6 @@ copy_bin()
     local from=$bin_dir/$bin_zip_template
     local to=$build_dir
     
-    echo $from $to
-    
     cp $from $to
     
     if [[ ! $? -eq 0 ]]; then
@@ -51,16 +51,33 @@ copy_bin()
 
 unzip_bin() {
     cd $build_dir
-    rm -rf unzip_dir && mkdir unzip_dir && sleep 30
-    unzip $bin_zip_template -d unzip_dir
+    rm -rf $unzip_bin_dir && mkdir $unzip_bin_dir
+    unzip $bin_zip_template -d $unzip_bin_dir
     cd $script_dir
 }
 
+clean_build_dir()
+{
+    rm -rf $build_dir
+}
+
+copy_docker_file_to_unzip_dir()
+{
+    cp $dockerfiles_dir/$docker_file_name $unzip_bin_dir
+}
+
+create_docker_image()
+{
+    docker build -t name $unzip_bin_dir
+}
+
+mkdir $build_dir
 
 if [[ ! -z $1 ]]; then
-    if [[ ! -d $1 ]]; then
-        bin_dir=$1
-        cd $bin_dir && bin_dir=$(pwd) && cd $script_dir
+    bin_dir=$1
+    cd $bin_dir
+    if [[ $? -eq 0 ]]; then
+        bin_dir=$(pwd) && cd $script_dir
         assembly_with_src=false
     else
         critical_message "ERROR. $1 is not a dir"
@@ -68,13 +85,19 @@ if [[ ! -z $1 ]]; then
     fi
 fi
 
-if [[ $assembly_with_src ]]; then
+if [[ $assembly_with_src == true ]]; then
     assembly_project
 fi
 
 copy_bin && unzip_bin
 
-echo $script_dir $root_dir $dockerfiles_dir
+copy_docker_file_to_unzip_dir
+
+create_docker_image
+
+clean_build_dir
+
+#echo $script_dir $root_dir $dockerfiles_dir
 
 
 
