e38cd3e9e2590c4d55203ba0e393d8bc67a5402d
DmitryZagr <bikemaster2000@list.ru>
2017-07-03 22:34:16 +0300

    change maven docker tag to latest

--- docker/scripts/assembly_bin_in_docker.sh
+++ docker/scripts/assembly_bin_in_docker.sh
@@ -6,7 +6,7 @@ root_dir=$script_dir/../..
 
 assembly_project()
 {
-	docker run -it --rm --name uss-asswmbly -v "$root_dir":/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-7 mvn clean package
+	docker run -it --rm --name uss-asswmbly -v "$root_dir":/usr/src/mymaven -w /usr/src/mymaven maven:latest mvn clean package
 }
 
 assembly_project
